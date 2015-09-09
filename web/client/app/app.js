431ddb9f5444560b32fc0f518250c7a761afc265
2remdou <2remdou@gmail.com>
2015-09-09 22:36:07 +0000

    ui.boostrap

--- web/client/app/app.js
+++ web/client/app/app.js
@@ -2,7 +2,8 @@
  * Created by touremamadou on 20/08/2015.
  */
 var app = angular.module('app',[
-    'ui.router'
+    'ui.router',
+    'ui.bootstrap.tabs'
 ]);
 
 app.config(function($interpolateProvider) {
