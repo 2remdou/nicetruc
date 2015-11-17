406a4781f09b69dcbc0bb0e77a1fe821f7db1412
mdoutoure <2remdou@gmail.com>
2015-11-17 17:23:52 +0000

    refresh MainController

---
+++ web/client/app/controllers/MainController.js
@@ -0,0 +1,24 @@
+/**
+ * Created by touremamadou on 12/09/2015.
+ */
+app.controller('MainController',['$scope','VoitureService','usSpinnerService','$state',
+    function($scope,VoitureService,usSpinnerService,$state)
+    {
+        usSpinnerService.spin('nt-spinner');
+
+        VoitureService.listVedette().then(function(response){
+            angular.forEach(response.data,function(voiture){
+                if(!voiture.imagePrincipale){
+                    voiture.imagePrincipale = {webPath: "client/app/images/voitures/defaultVoiture.png",imageName: "defaultVoiture.jpg"};
+                }
+            });
+            $scope.voitures=response.data;
+            usSpinnerService.stop('nt-spinner');
+        },function(){
+            usSpinnerService.stop('nt-spinner');
+        });
+
+        $scope.showVoiture = function(voitureId){
+            $state.go('nicetruc.showVoiture',{voitureId:voitureId});
+        };
+    }]);
