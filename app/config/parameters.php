4d80fd303c53e3d6fbaa6b4ca9e3a9e63d18d0a5
2remdou <2remdou@gmail.com>
2015-11-21 07:42:40 +0000

    env

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -5,10 +5,9 @@
  * Date: 18/11/2015
  * Time: 13:48
  */
-if(@$_ENV['MYSQL_ADDON_HOST']) {
-    $container->setParameter('database_host', $_ENV['MYSQL_ADDON_HOST']);
-    $container->setParameter('database_port', $_ENV['MYSQL_ADDON_PORT']);
-    $container->setParameter('database_name', $_ENV['MYSQL_ADDON_DB']);
-    $container->setParameter('database_user', $_ENV['MYSQL_ADDON_USER']);
-    $container->setParameter('database_password', $_ENV['MYSQL_ADDON_PASSWORD']);
-}
+
+$container->setParameter('database_host', $_ENV['SYMFONY__DB_HOST']);
+$container->setParameter('database_port', $_ENV['SYMFONY__DB_PORT']);
+$container->setParameter('database_name', $_ENV['SYMFONY__DB']);
+$container->setParameter('database_user', $_ENV['SYMFONY__DB_USER']);
+$container->setParameter('database_password', $_ENV['SYMFONY__DB_PASSWORD']);
