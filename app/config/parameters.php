9dc860800010d8d7bed2001cef5bfc7c814f7c48
2remdou <2remdou@gmail.com>
2015-11-21 09:50:07 +0000

    db

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -6,8 +6,7 @@
  * Time: 13:48
  */
 
-$container->setParameter('database_host', $_SERVER['MYSQL_HOST']);
-$container->setParameter('database_port', $_SERVER['MYSQL_ADDON_PORT']);
-$container->setParameter('database_name', $_SERVER['MYSQL_DB_NAME']);
+$container->setParameter('database_host', $_SERVER['MYSQL_ADDON_HOST']);
+$container->setParameter('database_name', $_SERVER['MYSQL_ADDON_DB']);
 $container->setParameter('database_user', $_SERVER['MYSQL_ADDON_USER']);
 $container->setParameter('database_password', $_SERVER['MYSQL_ADDON_PASSWORD']);
