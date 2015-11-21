874425a0bd449e17877a57d4ad69e32656ec2778
2remdou <2remdou@gmail.com>
2015-11-21 06:35:10 +0000

    init db

--- clevercloud/nicetruc.sh
+++ clevercloud/nicetruc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-php app\console doctrine:database:create
-php app\console doctrine:schema:update --force
+php app/console doctrine:database:create
+php app/console doctrine:schema:update --force
