c363c5e6af4f8d86db5b77c61d64a9fc687f2236
2remdou <2remdou@gmail.com>
2015-12-10 21:11:25 +0000

    config mailer

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -5,9 +5,10 @@
  * Date: 18/11/2015
  * Time: 13:48
  */
-if(isset($_SERVER['MYSQL_ADDON_HOST'])){
+
+/*if(isset($_SERVER['MYSQL_ADDON_HOST'])){
     $container->setParameter('database_host', $_SERVER['MYSQL_ADDON_HOST']);
     $container->setParameter('database_name', $_SERVER['MYSQL_ADDON_DB']);
     $container->setParameter('database_user', $_SERVER['MYSQL_ADDON_USER']);
-    $container->setParameter('database_password', $_SERVER['MYSQL_ADDON_PASSWOR']);
-}
+    $container->setParameter('database_password', $_SERVER['MYSQL_ADDON_PASSWORD']);
+}*/
