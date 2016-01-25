0e7ba19a6858ff5d1aed6fe10296e0c105e68eb0
2remdou <2remdou@gmail.com>
2016-01-25 19:50:24 +0000

    pagination

--- src/AppBundle/Controller/NiceTrucController.php
+++ src/AppBundle/Controller/NiceTrucController.php
@@ -62,63 +62,7 @@ class NiceTrucController extends FOSRestController
        return $this->indexAction($request);
     }
 
-
-    /**
-     * @Route("/villes",name="nicetruc_ville")
-     * @Security("has_role('ROLE_SUPER_ADMIN')")
-     */
-    public function villeAction(Request $request){
-        return $this->render('AppBundle::ville.html.twig');
-    }
-
-    /**
-     * @Route("/quartiers",name="nicetruc_quartier")
-     * @Security("has_role('ROLE_SUPER_ADMIN')")
-     */
-    public function quartierAction(Request $request){
-        return $this->render('AppBundle::quartier.html.twig');
-    }
-
-    /**
-     * @Route("/article",name="nicetruc_article")
-     * @Security("has_role('ROLE_USER')")
-     */
-    public function articleAction(Request $request){
-        return $this->render('AppBundle::article.html.twig');
-    }
-
-    /**
-     * @Route("/categorie",name="nicetruc_categorie")
-     * @Security("has_role('ROLE_SUPER_ADMIN')")
-     */
-    public function categorieAction(Request $request){
-        return $this->render('AppBundle::categorie.html.twig');
-    }
-
-    /**
-     * @Route("/boitier",name="nicetruc_boitier")
-     * @Security("has_role('ROLE_SUPER_ADMIN')")
-     */
-    public function BoitierAction(Request $request){
-        return $this->render('AppBundle::boitier.html.twig');
-    }
-
-    /**
-     * @Route("/carburant",name="nicetruc_carburant")
-     * @Security("has_role('ROLE_SUPER_ADMIN')")
-     */
-    public function CarburantAction(Request $request){
-        return $this->render('AppBundle::carburant.html.twig');
-    }
-
-    /**
-     * @Route("/marque",name="nicetruc_marque")
-     * @Security("has_role('ROLE_SUPER_ADMIN')")
-     */
-    public function MarqueAction(Request $request){
-        return $this->render('AppBundle::marque.html.twig');
-    }
-
+    
     /**
      * ajoute une image à une voiture
      *
