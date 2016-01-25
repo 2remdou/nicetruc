4a1aec2dfda6df8ac1adf80b0ca1a2239d9ab408
2remdou <2remdou@gmail.com>
2016-01-25 06:07:31 +0000

    retrait du bxslider

--- web/client/app/directives/ntBxSlider.js
+++ web/client/app/directives/ntBxSlider.js
@@ -5,35 +5,37 @@
 app.directive('ntBxSlider',['$timeout','$rootScope',function($timeout,$rootScope){
    return {
        restrict: 'A',
-       replace: true,
-       scope: {
-         voitures: '='
-       },
-       transclude: true,
        templateUrl: 'client/app/views/bxSlider.html',
        link : function(scope,element,attributes){
 
        			$rootScope.$on('parameters.completed.load',function(event,data){
 		            element.ready(function() {
 
+/*
 	            	$timeout(function() {
 						scope.$apply(function() {
 		                	scope.voitures = $rootScope.voituresEnVedette;
 		                	log(scope.voitures);
 		            	}); 
 					});
+*/
 
 					element.bxSlider({
 			              auto: true,
 				          pause: 3000,
 				          pager: false
-	          		});   
+	          		});
 		        });
 	            
 		             
 
 		            
             });
-       }
+       },
+	   controller : function($scope){
+		   $scope.cliquer = function(){
+			   log($scope);
+		   }
+	   }
    }
 }]);
